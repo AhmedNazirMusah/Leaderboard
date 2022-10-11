9d4be96715d8afaeff127ed85adc477cfe03a4c0
Ahmed Nazir Musah <ahmed999nazir@gmail.com>
2022-10-11 16:25:21 +0000

    project-setup

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # Leaderboard
+
+> LeaderBoard
+
+## Built With
+
+- HTML
+- JavaScript
+- CSS
+- webpack
+
+## Live Demo
+
+[Live Demo Link]()
+
+
+## Getting Started
+
+## Author
+
+👤 **Ahmed Nazir**
+
+- GitHub: [@ahmednazirmusah](https://github.com/ahmednazirmusah)
+- LinkedIn: [LinkedIn](https://linkedin.com/in/ahmednazirmusah)
+
+
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are highly welcomed!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
+## 📝 License
+
+This project is [MIT](./LICENSE) licensed.
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname, 'src', 'index.js'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  stats: {
+    colors: true,
+    version: false,
+    hash: false,
+    timings: false,
+    chunks: false,
+    chunkModules: false,
+  },
+};
